ca47b3428d74bec8c4407bc7403e15e51f28b0c3
Raul <rauldoss.moraes@gmail.com>
2024-11-15 17:44:59 -0300

    update transform to relative method

--- assets/static/js/utils.js
+++ assets/static/js/utils.js
@@ -1,42 +1,48 @@
-const projectName = 'match-summary'
+$(document).ready(function () {
+    const projectName = 'match-summary'
 
-function formatLiveTime (totalSeconds) {
-    const minutes = Math.floor(totalSeconds / 60)
-    const seconds = totalSeconds % 60
+    function formatLiveTime (totalSeconds) {
+        const minutes = Math.floor(totalSeconds / 60)
+        const seconds = totalSeconds % 60
 
-    return `${minutes}:${String(seconds).padStart(2, '0')}`
-}
-
-function sortObjectByAttribute(obj, attribute, desc = true) {
-    return Object.values(obj).sort((a, b) => {
-        const valueA = a[attribute]
-        const valueB = b[attribute]
-        return desc ? valueB - valueA : valueA - valueB
-    })
-}
-
-function transformToRelative() {
-    const segments = window.location.pathname.substring(1).split('/').filter(segment => segment !== '')
-    const index = segments.indexOf(projectName)
-
-    if (index !== -1) {
-        $('a[href]').each(function() {
-            var currentHref = $(this).attr('href')
-
-            console.log($(this), currentHref)
-
-            var newHref = `/${ projectName }/${ currentHref }`
-
-            $(this).attr('href', newHref)
-        })
-
-        return `/${ projectName }/`
+        return `${minutes}:${String(seconds).padStart(2, '0')}`
     }
 
-    return segments.length > 0 ? '../'.repeat(segments.length) : ''
-}
+    function sortObjectByAttribute(obj, attribute, desc = true) {
+        return Object.values(obj).sort((a, b) => {
+            const valueA = a[attribute]
+            const valueB = b[attribute]
+            return desc ? valueB - valueA : valueA - valueB
+        })
+    }
 
-const relativePath = transformToRelative()
+    function transformToRelative() {
+        const segments = window.location.pathname.substring(1).split('/').filter(segment => segment !== '')
+        const index = segments.indexOf(projectName)
+
+        if (index !== -1) {
+            $('a[href]').each(function() {
+                var currentHref = $(this).attr('href')
+
+                console.log($(this), currentHref)
+
+                var newHref = `/${ projectName }/${ currentHref }`
+
+                $(this).attr('href', newHref)
+            })
+
+            return `/${ projectName }/`
+        }
+
+        return segments.length > 0 ? '../'.repeat(segments.length) : ''
+    }
+
+    const relativePath = transformToRelative()
+
+    window.relativePath = relativePath
+    window.formatLiveTime = formatLiveTime
+    window.sortObjectByAttribute = sortObjectByAttribute
+})
 
 // function sortObjectByAttribute(obj, attribute, desc = true) {
 //     return Object.fromEntries(
